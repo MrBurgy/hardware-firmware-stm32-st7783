9e9faf3e866a7cd5cfde71e2432f8f8e8f52a57d
volthouse <lucasgaertner@arcor.de>
2014-11-01 19:47:01 +0100

    Import blink2 as startup project.

---
+++ blinky2/src/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "BlinkLed.h"
+
+#include "stm32f4xx_hal.h"
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+int main(int argc, char* argv[])
+{
+
+  blink_led_init();
+
+  // Infinite loop
+  while (1)
+    {
+      blink_led_on();
+      HAL_Delay(500);
+      blink_led_off();
+      HAL_Delay(500);
+    }
+  // Infinite loop, never return.
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
