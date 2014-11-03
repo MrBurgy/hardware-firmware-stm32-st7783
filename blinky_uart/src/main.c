f03fdac1448d6856d029fc6cfa5be95c285d78a1
volthouse <pdpas@arcor.de>
2014-11-03 20:00:02 +0100

    add main.h

--- blinky_uart/src/main.c
+++ blinky_uart/src/main.c
@@ -10,26 +10,13 @@
  */
 
 #include <stdio.h>
+#include "main.h"
 #include "BlinkLed.h"
 
 #include "stm32f4xx_hal_conf.h"
 #include "stm32f4xx_hal.h"
 
-#define USARTx                           USART2
-#define USARTx_CLK_ENABLE()              __USART2_CLK_ENABLE();
-#define USARTx_RX_GPIO_CLK_ENABLE()      __GPIOA_CLK_ENABLE()
-#define USARTx_TX_GPIO_CLK_ENABLE()      __GPIOA_CLK_ENABLE()
 
-#define USARTx_FORCE_RESET()             __USART2_FORCE_RESET()
-#define USARTx_RELEASE_RESET()           __USART2_RELEASE_RESET()
-
-/* Definition for USARTx Pins */
-#define USARTx_TX_PIN                    GPIO_PIN_2
-#define USARTx_TX_GPIO_PORT              GPIOA
-#define USARTx_TX_AF                     GPIO_AF7_USART2
-#define USARTx_RX_PIN                    GPIO_PIN_3
-#define USARTx_RX_GPIO_PORT              GPIOA
-#define USARTx_RX_AF                     GPIO_AF7_USART2
 
 // ----- main() ---------------------------------------------------------------
 
